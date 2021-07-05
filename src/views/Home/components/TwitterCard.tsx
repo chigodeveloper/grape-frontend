016364ac1c3b0f6cacd2ae87f8e732eb3c334bd7
chigodeveloper <86495407+chigodeveloper@users.noreply.github.com>
2021-07-06 05:22:34 +0630

    updated home page

--- src/views/Home/components/TwitterCard.tsx
+++ src/views/Home/components/TwitterCard.tsx
@@ -35,7 +35,7 @@ const TwitterCard = () => {
         <Timeline
           dataSource={{
             sourceType: 'profile',
-            screenName: 'GooseFinance'
+            screenName: 'binance'
           }}
           options={{
             height: '300',
