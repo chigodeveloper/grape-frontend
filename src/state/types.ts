f18d7c802c061b6ae7b322e9834e50088fe06a9b
chigodeveloper <86495407+chigodeveloper@users.noreply.github.com>
2021-07-06 03:29:00 +0630

    reverseupdate

--- src/state/types.ts
+++ src/state/types.ts
@@ -8,7 +8,7 @@ export interface Farm extends FarmConfig {
   tokenPriceVsQuote?: BigNumber
   poolWeight?: number
   depositFeeBP?: number
-  TokensPerBlock?: number
+  eggPerBlock?: number
     userData?: {
     allowance: BigNumber
     tokenBalance: BigNumber
