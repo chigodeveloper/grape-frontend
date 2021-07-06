3a0f3552f30605c4155b6b9408ad889942863380
chigodeveloper <86495407+chigodeveloper@users.noreply.github.com>
2021-07-07 02:26:36 +0630

    Update index.tsx

--- src/views/Referrals/index.tsx
+++ src/views/Referrals/index.tsx
@@ -64,7 +64,7 @@ const Referrals: React.FC = () => {
         ) : (
           <div>
             <Cards>
-              <TotalReferralCard />
+              <TotalCommissionCard />
             </Cards>
             <MyReferralLinkCard />
           </div>
