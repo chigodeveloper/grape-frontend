c9834b66b6c68a0dd03ad3146cc8602e2848c87c
chigodeveloper <86495407+chigodeveloper@users.noreply.github.com>
2021-07-07 01:09:32 +0630

    added get and save referral

--- src/state/hooks.ts
+++ src/state/hooks.ts
@@ -1,10 +1,13 @@
 import BigNumber from 'bignumber.js'
+import { ethers } from 'ethers'
 import { useEffect, useMemo } from 'react'
 import { useSelector, useDispatch } from 'react-redux'
 import useRefresh from 'hooks/useRefresh'
 import { fetchFarmsPublicDataAsync, fetchPoolsPublicDataAsync, fetchPoolsUserDataAsync } from './actions'
 import { State, Farm, Pool } from './types'
 import { QuoteToken } from '../config/constants/types'
+import { useWallet } from '@binance-chain/bsc-use-wallet'
+import fetchReferralInfo from './referrals/fetchReferralsInfo'
 
 const ZERO = new BigNumber(0)
 
@@ -104,4 +107,29 @@ export const useTotalValue = (): BigNumber => {
     }
   }
   return value;
+}
+
+const saveReferrer = async (account, ref) => {
+  if (!ethers.utils.isAddress(ref) || account === ref) {
+    return
+  }
+  const referralData = await fetchReferralInfo(account)
+  if (referralData.referrer === '0x0000000000000000000000000000000000000000') {
+    localStorage.setItem('REFERRER', ref)
+  }
+}
+export const useSaveReferrer = () => {
+  // eslint-disable-next-line
+  const search = window.location.search
+  const ref = new URLSearchParams(search).get('ref')
+  const { account } = useWallet()
+  useEffect(() => {
+    if (account && ref) {
+      saveReferrer(account, ref)
+    }
+  }, [account, ref])
+}
+
+export const useGetReferralInfo = () => {
+  return useSelector((state: State) => state.referrals.data)
 }
